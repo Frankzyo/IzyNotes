f9ee18ac8d0a06ba7af03c105c1ab9ff7587db99
JUCHA <rodrigojuchasara1@gmail.com>
2024-12-16 09:48:44 -0400

    Funcion de guardar info de sesion

--- app/src/main/java/com/programovil/izynotes/Inicio.kt
+++ app/src/main/java/com/programovil/izynotes/Inicio.kt
@@ -1,5 +1,6 @@
 package com.programovil.izynotes
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -14,11 +15,24 @@ class Inicio : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
 
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.apply()
+
         setup()
     }
     private fun setup(){
         val botonCerrar: Button = findViewById(R.id.cerrarSesion)
         botonCerrar.setOnClickListener {
+
+            //Borado de datos
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
--- app/src/main/java/com/programovil/izynotes/login.kt
+++ app/src/main/java/com/programovil/izynotes/login.kt
@@ -1,18 +1,28 @@
 package com.programovil.izynotes
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class login : AppCompatActivity() {
+
+    private val GOOGLE_SING_IN = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +35,27 @@ class login : AppCompatActivity() {
         }
 
         setup()
+        sesion()
+
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        val ventana = findViewById<LinearLayout>(R.id.authLayout)
+        ventana.visibility = View.VISIBLE
+    }
+
+    private fun sesion(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+
+        if(email != null){
+            val ventana = findViewById<LinearLayout>(R.id.authLayout)
+            ventana.visibility = View.INVISIBLE
+            showInicio(email)
+
+        }
 
     }
 
@@ -57,6 +88,18 @@ class login : AppCompatActivity() {
                 showAlert("Por favor, complete todos los campos.")
             }
         }
+
+        val googleBoton= findViewById<ImageButton>(R.id.imageButton3)
+        googleBoton.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SING_IN)
+        }
     }
 
     private fun showAlert(message: String = "Se ha producido un error autenticando al usuario") {
@@ -73,5 +116,34 @@ class login : AppCompatActivity() {
             putExtra("email", email)
         }
         startActivity(inicioIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SING_IN){
+            val  task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+
+                            if (it.isSuccessful) {
+                                showInicio(account.email ?: "")
+                            } else {
+                                showAlert()
+                            }
+                        }
+
+                }
+            }catch (e: ApiException){
+                showAlert()
+            }
+        }
     }
 }
